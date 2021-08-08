47fad11f27e029d42e630db269717db79f1fde5e
Samik-coder <67789023+Samik-coder@users.noreply.github.com>
2021-08-09 00:03:28 +0530

    confidencial xDDD

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MongoURI: "mongodb+srv://samik:samik@cluster0.2prrc.mongodb.net/Force-Fest",
+  MongoURI2:
+    "mongodb+srv://samik:samik@cluster0.2prrc.mongodb.net/Force-Fest-to-do",
+};
